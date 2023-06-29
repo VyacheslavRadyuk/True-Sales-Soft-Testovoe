f1f619e81a5d339e4e0f4abd6c74ec5620082f22
Vyacheslav Radyuk <vyacheslav.radyuk@jetbi.com>
2023-06-29 22:17:08 +0300

    added callout and product update methode

--- force-app/main/default/aura/OrderManagementQuickAction/OrderManagementQuickActionController.js
+++ force-app/main/default/aura/OrderManagementQuickAction/OrderManagementQuickActionController.js
@@ -2,26 +2,6 @@
     navigateToLC : function(component, event, helper) {
         $A.get("e.force:closeQuickAction").fire();
 
-        // var pageReference = {
-        //     type: 'standard__component',
-        //     attributes: {
-        //         componentName: 'c__OrderManagementMain'
-        //     },
-        //     state: {
-        //         c__refRecordId: component.get("v.recordId")
-        //     }
-        // };
-        // component.set("v.pageReference", pageReference);
-        // const navService = component.find('navService');
-        // const pageRef = component.get('v.pageReference');
-        // const handleUrl = (url) => {
-        //     window.open(url);
-        // };
-        // const handleError = (error) => {
-        //     console.log(error);
-        // };
-        // navService.generateUrl(pageRef).then(handleUrl, handleError);
-
         var pageReference = {
             type: 'standard__navItemPage',
             attributes: {
--- force-app/main/default/lwc/orderManagement/orderManagement.js
+++ force-app/main/default/lwc/orderManagement/orderManagement.js
@@ -1,14 +1,35 @@
-import { LightningElement, api, wire } from 'lwc';
-import { CurrentPageReference, NavigationMixin } from 'lightning/navigation'
+import { LightningElement, api, wire, track } from 'lwc';
+import { CurrentPageReference, NavigationMixin } from 'lightning/navigation';
+import initData from '@salesforce/apex/OrderManagementController.getInitData';
 
 export default class OrderManagement extends NavigationMixin(LightningElement) {
-    @api recordId;
     @api refRecordId;
+    @track account;
+    @track isManager;
+
+    get isDisabledButton() {
+        return !this.isManager;
+    }
 
     @wire(CurrentPageReference)
     setCurrentPageReference(currentPageReference) {
         if (currentPageReference.state.c__refRecordId) {
             this.refRecordId = currentPageReference.state.c__refRecordId;
         }
+    }
+
+    @wire(initData, {recordId : '$refRecordId'})  
+    wiredInitData({error, data}) {
+        if(data) {
+            this.account = data.currentAccount;
+            this.isManager = data.isManager;
+
+        } else if (error) {
+            console.log(error);
+        }
+    }
+
+    createProduct() {
+        
     }
 }
