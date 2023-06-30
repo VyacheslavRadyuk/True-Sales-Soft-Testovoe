74bb9796a2f005a71c60b88e94c89851d76cfd0f
Vyacheslav Radyuk <vyacheslav.radyuk@jetbi.com>
2023-06-30 09:58:37 +0300

    create a form for creating a new product

--- force-app/main/default/lwc/orderManagement/orderManagement.js
+++ force-app/main/default/lwc/orderManagement/orderManagement.js
@@ -2,11 +2,24 @@ import { LightningElement, api, wire, track } from 'lwc';
 import { CurrentPageReference, NavigationMixin } from 'lightning/navigation';
 import initData from '@salesforce/apex/OrderManagementController.getInitData';
 import createProduct from '@salesforce/apex/OrderManagementController.createProduct';
+import PRODUCT_OBJECT from '@salesforce/schema/Product__c';
+import NAME_FIELD from '@salesforce/schema/Product__c.Name';
+import TYPE_FIELD from '@salesforce/schema/Product__c.Type__c';
+import FAMILY_FIELD from '@salesforce/schema/Product__c.Family__c';
+import IMAGE_FIELD from '@salesforce/schema/Product__c.Image__c';
+import DESCRIPTION_FIELD from '@salesforce/schema/Product__c.Description__c';
 
 export default class OrderManagement extends NavigationMixin(LightningElement) {
+    productObject = PRODUCT_OBJECT;
+    nameField = NAME_FIELD;
+    typeField = TYPE_FIELD;
+    familyField = FAMILY_FIELD;
+    imageField = IMAGE_FIELD;
+    descriptionField = DESCRIPTION_FIELD;
     @api refRecordId;
     @track account;
     @track isManager;
+    @track isModalOpen = false;
 
     get isDisabledButton() {
         return !this.isManager;
@@ -30,7 +43,15 @@ export default class OrderManagement extends NavigationMixin(LightningElement) {
         }
     }
 
+    openModal() {
+        this.isModalOpen = true;
+    }
+
+    closeModal() {
+        this.isModalOpen = false;
+    }
+
     createProduct() {
-        
+        this.isModalOpen = false;
     }
 }
