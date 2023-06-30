34f122f045ecc30820de435cf2c980a00ac02648
Vyacheslav Radyuk <vyacheslav.radyuk@jetbi.com>
2023-06-30 12:13:04 +0300

    pass params from ui form for creating product

--- force-app/main/default/lwc/orderManagement/orderManagement.js
+++ force-app/main/default/lwc/orderManagement/orderManagement.js
@@ -12,10 +12,15 @@ import DESCRIPTION_FIELD from '@salesforce/schema/Product__c.Description__c';
 export default class OrderManagement extends NavigationMixin(LightningElement) {
     productObject = PRODUCT_OBJECT;
     nameField = NAME_FIELD;
+    nameFieldValue;
     typeField = TYPE_FIELD;
+    typeFieldValue;
     familyField = FAMILY_FIELD;
+    familyFieldValue
     imageField = IMAGE_FIELD;
+    imageFieldValue;
     descriptionField = DESCRIPTION_FIELD;
+    descriptionFieldValue;
     @api refRecordId;
     @track account;
     @track isManager;
@@ -52,6 +57,38 @@ export default class OrderManagement extends NavigationMixin(LightningElement) {
     }
 
     createProduct() {
+        createProduct({productName: this.nameFieldValue, productType: this.typeFieldValue, productDescription: this.descriptionFieldValue, productFamily: this.familyFieldValue, productImage: this.imageFieldValue})
+        .then(result => {
+        })
+        .catch(error => {
+            this.error = error;
+            console.error(error.body.message);
+        });
+        this.nameFieldValue = '';
+        this.typeFieldValue = '';
+        this.familyFieldValue = '';
+        this.descriptionFieldValue = '';
+        this.imageFieldValue = '';
         this.isModalOpen = false;
+    }
+
+    handleChangeFieldName(event) {
+        this.nameFieldValue = event.target.value;
+    }
+
+    handleChangeFieldType(event) {
+        this.typeFieldValue = event.target.value;
+    }
+
+    handleChangeFieldFamily(event) {
+        this.familyFieldValue = event.target.value;
+    }
+
+    handleChangeFieldImage(event) {
+        this.imageFieldValue = event.target.value;
+    }
+
+    handleChangeFieldDescription(event) {
+        this.descriptionFieldValue = event.target.value;
     }
 }
